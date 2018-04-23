25289dab254ea2b61a386347151f8c04362441f9
Gautam Mehra <gautammehra@Gautams-MacBook-Pro.local>
2018-04-23 00:12:36 -0700

    changing to different images

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PhotoGallery from './PhotoGallery'
-
+import './app.css'
 export default class App extends React.Component {
 	constructor(){
 		super();
@@ -9,13 +9,13 @@ export default class App extends React.Component {
 	componentDidMount() {
 		// most of the times you would load this dynamic data throigh an ejax call in this function
 	    this.setState({
-	    	imageGallery: [{img:'https://picsum.photos/200/300', caption:'I am image 1'},{img:'https://picsum.photos/200/300', caption:'I am image 2'},{img:'https://picsum.photos/200/300', caption:'I am image 3'},{img:'https://picsum.photos/200/300', caption:'I am image 4'},{img:'https://picsum.photos/200/300', caption:'I am image 5'},{img:'https://picsum.photos/200/300', caption:'I am image 6 of infinite gallery'}]
+	    	imageGallery: [{img:'https://picsum.photos/200/300?image=67', caption:'I am image 1'},{img:'https://picsum.photos/200/300?image=54', caption:'I am image 2'},{img:'https://picsum.photos/200/300?image=104', caption:'I am image 3'},{img:'https://picsum.photos/200/300?image=100', caption:'I am image 4'},{img:'https://picsum.photos/200/300?image=22', caption:'I am image 5'},{img:'https://picsum.photos/200/300?image=25', caption:'I am image 6 of infinite gallery'}]
 	    })
 	}
     render() {
     	let {imageGallery} = this.state
         return (
-            <div>
+            <div >
             	{
             		(imageGallery)? <PhotoGallery imageGallery={imageGallery}/>: undefined
             	}
