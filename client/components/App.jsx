6a4f4b12abd712c0b233c240c14a81bc471f9885
Gautam Mehra <gautammehra@Gautams-MacBook-Pro.local>
2018-04-24 20:56:46 -0700

    adding a font family

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,7 +9,7 @@ export default class App extends React.Component {
 	componentDidMount() {
 		// most of the times you would load this dynamic data throigh an ejax call in this function
 	    this.setState({
-	    	imageGallery: [{img:'https://picsum.photos/200/300?image=67', caption:'I am image 1'},{img:'https://picsum.photos/200/300?image=54', caption:'I am image 2'},{img:'https://picsum.photos/200/300?image=104', caption:'I am image 3'},{img:'https://picsum.photos/200/300?image=100', caption:'I am image 4'},{img:'https://picsum.photos/200/300?image=22', caption:'I am image 5'},{img:'https://picsum.photos/200/300?image=25', caption:'I am image 6 of infinite gallery'}]
+	    	imageGallery: [{img:'https://picsum.photos/200/300?image=67', caption:'I am image 1'},{img:'https://picsum.photos/200/300?image=54', caption:'I am image 2'},{img:'https://picsum.photos/200/300?image=104', caption:'I am image 3'},{img:'https://picsum.photos/200/300?image=100', caption:'I am image 4'},{img:'https://picsum.photos/200/300?image=22', caption:'I am image 5'},{img:'https://picsum.photos/200/300?image=25', caption:'I am image 6'}]
 	    })
 	}
     render() {
